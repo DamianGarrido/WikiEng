f47a1c346d3338666be070f52af3f94dcf9b9bda
Damian Garrido <dgarrido@soluciones.com.ar>
2019-04-26 17:54:48 -0300

    Add campoModificableDesdeLaGrilla.sql

---
+++ src/Grillas/campoModificableDesdeLaGrilla.sql
@@ -0,0 +1,10 @@
+-- ==============================================================
+-- CAMPO MODIFICABLE DESDE LA GRILLA CON UN INPUT TEXT
+-- ==============================================================
+
+SELECT 
+'<input type="text" onchange="almacenarSeleccion(''CAMPO'',event.target.value);SaltoaEstructura(''9999_9'');" value="'+T.VALOR+'"/>' AS 'Campo'
+
+FROM TABLA T
+
+-- El salto a sstructura tiene que ir a una transaccion que haga un UPDATE a la tabla a modificar con el nuevo valor.
